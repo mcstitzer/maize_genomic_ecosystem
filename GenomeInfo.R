2ef504c32a184e32a163cb16116b7deab1bc67b3
mcstitzer <mcstitzer@ucdavis.edu>
2018-08-30 16:35:37 -0700

    Create GenomeInfo.R

---
+++ GenomeInfo.R
@@ -0,0 +1,14 @@
+## loads TEFILE, TEDISJOINED, GENOMENAME, SHORTID, GENOMEFA
+
+## the path to the entire TE file (including the filename)
+TEFILE=''
+
+## the path to the disjoined TE file (including the filename)
+TEDISJOINED=''
+
+## names of the genome and its short ID assigned by the maizegdb nomenclature committee (https://www.maizegdb.org/nomenclature)
+GENOMENAME='B73'
+SHORTID='Zm00001d'
+
+## the path to the genome fasta (including the filename)
+GENOMEFA=''
