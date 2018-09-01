5e61694883e722c46c7ebad964e5057b3124b7e9
mcstitzer <mcstitzer@ucdavis.edu>
2018-09-01 13:56:17 -0700

    v2 te annotations

--- GenomeInfo.R
+++ GenomeInfo.R
@@ -1,14 +1,14 @@
 ## loads TEFILE, TEDISJOINED, GENOMENAME, SHORTID, GENOMEFA
 
 ## the path to the entire TE file (including the filename)
-TEFILE=''
+TEFILE='/home/mstitzer/projects/agpv4_te_annotation/ncbi_pseudomolecule/B73.structuralTEv2.2018-08-31.gff3.gz'
 
 ## the path to the disjoined TE file (including the filename)
-TEDISJOINED=''
+TEDISJOINED='/home/mstitzer/projects/agpv4_te_annotation/ncbi_pseudomolecule/B73.structuralTEv2.2018-08-31.disjoined.gff3.gz'
 
 ## names of the genome and its short ID assigned by the maizegdb nomenclature committee (https://www.maizegdb.org/nomenclature)
 GENOMENAME='B73'
 SHORTID='Zm00001d'
 
 ## the path to the genome fasta (including the filename)
-GENOMEFA=''
+GENOMEFA='/home/mstitzer/projects/agpv4_te_annotation/ncbi_pseudomolecule/B73V4.both_pseudo_AND_unplaced.fa'
