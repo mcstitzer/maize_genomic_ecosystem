d5547e36e4cbe323981dbc36dec818c090bdd140
Michelle Stitzer <mcstitzer@ucdavis.edu>
2018-09-20 14:23:53 -0700

    changes

--- GenomeInfo.R
+++ GenomeInfo.R
@@ -1,13 +1,13 @@
 ## loads TEFILE, TEDISJOINED, GENOMENAME, SHORTID, GENOMEFA
 
 ## the path to the entire TE file (including the filename)
-TEFILE='/home/mstitzer/projects/agpv4_te_annotation/ncbi_pseudomolecule/B73.structuralTEv2.2018-08-31.gff3.gz'
+TEFILE='/home/mstitzer/projects/maize_genomic_ecosystem/B73.2018-09-19.allTE.gff3'
 
 ## the path to the disjoined TE file (including the filename)
-TEDISJOINED='/home/mstitzer/projects/agpv4_te_annotation/ncbi_pseudomolecule/B73.structuralTEv2.2018-08-31.disjoined.gff3.gz'
+TEDISJOINED='/home/mstitzer/projects/maize_genomic_ecosystem/B73.2018-09-19.allTE.disjoined.gff3'
 
 ## names of the genome and its short ID assigned by the maizegdb nomenclature committee (https://www.maizegdb.org/nomenclature)
-GENOMENAME='B73'
+GENOME='B73'
 SHORTID='Zm00001d'
 
 ## the path to the genome fasta (including the filename)
