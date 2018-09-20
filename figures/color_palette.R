dbc1c7c31dabc0729b242304d26c166ba6b454e8
Michelle Stitzer <mcstitzer@ucdavis.edu>
2018-09-20 14:21:52 -0700

    colors

---
+++ figures/color_palette.R
@@ -0,0 +1,21 @@
+
+
+
+tecolors=c("#c04673",
+"#826061",
+"#c6773c",
+"#9783b5",
+"#6c4da4",
+"#a34bd6","#a9ce40",
+"#8fa657",
+"#657563",
+"#68c29b",
+"#6ea5c0",
+"#4771be","#ed3725")
+
+names(tecolors)=c('RLC', 'RIL', 'RLX', 'RST', 'RIT', 'RLG', 
+					'DTA', 'DTC', 'DTH', 'DTM', 'DTX', 'DTT', 'DHH')
+          
+dd.col=tecolors  
+
+TESUPFACTORLEVELS=c('DHH', 'DTA', 'DTC', 'DTH', 'DTM', 'DTT', 'DTX', 'RLC', 'RLG', 'RLX', 'RIT', 'RIL', 'RST')        
