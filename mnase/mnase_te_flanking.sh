16ad3f90ffeb21dff16d45bb0cc751dbf312e90f
mcstitzer <mcstitzer@ucdavis.edu>
2018-09-20 14:41:43 -0700

    add flanking kb

--- mnase/mnase_te_flanking.sh
+++ mnase/mnase_te_flanking.sh
@@ -1,16 +1,27 @@
 
-source('../GenomeInfo.sh')
+source('../GenomeInfo.R') ## works as bash as well as R
+
 
 
 ## find overlaps with TEs
-bedtools intersect -names AP RP -wo -a $TEDISJOINED -b AP.bfthresh1.1.MNaseHS.Ranges.AGPv4.bed RP.bfthresh1.1.MNaseHS.Ranges.AGPv4.bed > B73v4_MNaseHS.bedout.txt
+bedtools intersect -names AP RP -wo -a $TEDISJOINED -b AP.bfthresh1.1.MNaseHS.Ranges.AGPv4.bed RP.bfthresh1.1.MNaseHS.Ranges.AGPv4.bed > ${GENOME}_MNaseHS.bedout.txt
 
 ### make a file that includes just the 1kb region flanking each TE
-FLANKINGFILE=
+if [ ! -f ${GENOMEFA}.fai ]
+then
+samtools faidx ${GENOMEFA}
+fi
+
+if [ ! -f ${GENOME}.allTE.FLANK1kbEACH.gff3 ]
+then
+bedtools flank -b 1000 -i $TEFILE -g ${GENOMEFA}.fai > ${GENOME}.allTE.FLANK1kbEACH.gff3 
+fi
+
+FLANKGFF=${GENOME}.allTE.FLANK1kbEACH.gff3
 
 
 ## get overlaps with flanking regions
-bedtools intersect -names AP RP -wo -a $FLANKINGFILE -b AP.bfthresh1.1.MNaseHS.Ranges.AGPv4.bed RP.bfthresh1.1.MNaseHS.Ranges.AGPv4.bed > B73v4_MNaseHS.flank.bedout.txt
+bedtools intersect -names AP RP -wo -a $FLANKGFF -b AP.bfthresh1.1.MNaseHS.Ranges.AGPv4.bed RP.bfthresh1.1.MNaseHS.Ranges.AGPv4.bed > ${GENOME}_MNaseHS.flank.bedout.txt
 
 
 
