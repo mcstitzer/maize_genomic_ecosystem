286a6dbbe4dbf746bc9f0aae3fb9f2d5e8e4684a
mcstitzer <mcstitzer@ucdavis.edu>
2018-10-11 22:07:44 -0700

    Create get_terminal_bp_sups.sh

---
+++ te_age/tbl_age/get_terminal_bp_sups.sh
@@ -0,0 +1,8 @@
+python get_terminal_bp.py RLG.fa 1000 > RLG.1000.fa
+
+python get_terminal_bp.py RLC.fa 1000 > RLC.1000.fa
+
+python get_terminal_bp.py RLX.fa 1000 > RLX.1000.fa
+
+python get_terminal_bp.py DHH.fa -30 > DHH.last30.fa
+
