b6b22b8a6f298500a868d24055d98665ea7df29a
mcstitzer <mcstitzer@ucdavis.edu>
2018-10-11 22:59:06 -0700

    Create filter_transdecoder.R

---
+++ te_genes/orfs/filter_transdecoder.R
@@ -0,0 +1,21 @@
+library(rtracklayer)
+library(dplyr)
+
+## read in this transdecoder file (which only contains the LONGEST orf for each)
+## note that there is a minimum ORF length of 100 aa, so some TEs won't have an ORF called.
+a=import.gff3('B73v4.Zm00001d.allTE.fa.transdecoder.gff3')
+b=a[a$type=='CDS',]
+
+## make a new df that only has orf length
+orf=data.frame(TEID=as.character(seqnames(b)), orfAA=width(b)/3)
+
+write.table(orf, 'B73v4_allTE_orf.txt', sep='\t', quote=F, row.names=F, col.names=T)
+
+orf$fam=substr(orf$TEID, 1,8)
+orf$sup=substr(orf$TEID,1,3)
+
+orf_fam=orf %>% group_by(sup, fam) %>% dplyr::summarize(avg_orfAA=mean(orfAA, na.rm=T))
+write.table(orf_fam, 'B73v4_allTE_orf.fam.txt', sep='\t', quote=F, row.names=F, col.names=T)
+
+orf_sup=orf_fam %>% group_by(sup) %>% dplyr::summarize(avg_orfAA=mean(avg_orfAA, na.rm=T))
+write.table(orf_sup, 'B73v4_allTE_orf.sup.txt', sep='\t', quote=F, row.names=F, col.names=T)
