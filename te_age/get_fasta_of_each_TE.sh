253b4c9c0d69508a5c8de0be9ea1783f37f5564b
Michelle Stitzer <mcstitzer@ucdavis.edu>
2018-10-11 22:04:30 -0700

    to fasta

--- te_age/get_fasta_of_each_TE.sh
+++ te_age/get_fasta_of_each_TE.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 source ../GenomeInfo.R
 
 ## note these are tabs!
-awk '{print $1 "  " $4 "  " $5 "  " substr($9,4,24)}' $TEDISJOINED > $TEDISJOINED.tofasta.bed
+awk 'NF<9 {next} {print $1 "	" $4 "	" $5 "	" substr($9,4,21)}' $TEDISJOINED > $GENOME.tofasta.bed
 
-bedtools getfasta -name -fi $GENOMEFA -bed $TEDISJOINED.tofasta.bed -fo $GENOME.disjoined.fa
+bedtools getfasta -name -fi $GENOMEFA -bed $GENOME.tofasta.bed -fo $GENOME.disjoined.fa
 
 ## combine into one sequence per TEID
-$PYTHON2 collapse_chromosomes.py $GENOME.disjoined.fa > $GENOME.eachTE.fa
+python collapse_chromosomes.py $GENOME.disjoined.fa > $GENOME.eachTE.fa
