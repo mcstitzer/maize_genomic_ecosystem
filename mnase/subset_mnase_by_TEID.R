f89df14fc700a91b7f3dc8912777bd79d570055e
mcstitzer <mcstitzer@ucdavis.edu>
2018-10-23 14:59:28 -0700

    can't sum te_bp across copies!

--- mnase/subset_mnase_by_TEID.R
+++ mnase/subset_mnase_by_TEID.R
@@ -9,10 +9,10 @@ m$TEID=substr(m$V9, 4,24)
 flank=fread('B73_MNaseHS.flank.bedout.txt')
 flank$TEID=substr(flank$V9,4,24)
 
-mr=filter(m, V10=='RP') %>% group_by(TEID) %>% summarize(te_bp=sum(V5-V4+1, na.rm=T), n_root_hs=n(), root_bp=sum(V14))
-ms=filter(m, V10=='AP') %>% group_by(TEID) %>% summarize(te_bp=sum(V5-V4+1, na.rm=T), n_shoot_hs=n(), shoot_bp=sum(V14))
-mrf=filter(flank, V10=='RP') %>% group_by(TEID) %>% summarize(flank_bp=sum(V5-V4+1, na.rm=T), flank_n_root_hs=n(), flank_root_bp=sum(V14))
-msf=filter(flank, V10=='AP') %>% group_by(TEID) %>% summarize(flank_bp=sum(V5-V4+1, na.rm=T), flank_n_shoot_hs=n(), flank_shoot_bp=sum(V14))
+mr=filter(m, V10=='RP') %>% group_by(TEID) %>% summarize(te_bp=mean(V5-V4+1, na.rm=T), n_root_hs=n(), root_bp=sum(V14))
+ms=filter(m, V10=='AP') %>% group_by(TEID) %>% summarize(te_bp=mean(V5-V4+1, na.rm=T), n_shoot_hs=n(), shoot_bp=sum(V14))
+mrf=filter(flank, V10=='RP') %>% group_by(TEID) %>% summarize(flank_bp=mean(V5-V4+1, na.rm=T), flank_n_root_hs=n(), flank_root_bp=sum(V14))
+msf=filter(flank, V10=='AP') %>% group_by(TEID) %>% summarize(flank_bp=mean(V5-V4+1, na.rm=T), flank_n_shoot_hs=n(), flank_shoot_bp=sum(V14))
 
 mnase=merge(mr, ms, all=T)
 mnase=merge(mnase, mrf, all=T)
@@ -23,7 +23,14 @@ mnase$root_prop=mnase$root_bp/mnase$te_bp
 mnase$shoot_prop=mnase$shoot_bp/mnase$te_bp
 mnase$flank_root_prop=mnase$flank_root_bp/mnase$flank_bp
 mnase$flank_shoot_prop=mnase$flank_shoot_bp/mnase$flank_bp
+
+## for the TEs with no overlaps, want to include them in this file
+allte=fread('../te_characteristics/B73_TE_individual_copies.2018-09-19.txt')
+
+
+mnase=merge(mnase, allte[,'TEID'], all=T)
 mnase[is.na(mnase)]=0
+
 
 mnase$sup=substr(mnase$TEID,1,3)
 mnase$fam=substr(mnase$TEID,1,8)
