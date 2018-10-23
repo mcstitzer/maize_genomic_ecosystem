705b74e1fb6a8a61b9958979da1c73939d311805
mcstitzer <mcstitzer@ucdavis.edu>
2018-10-23 13:33:58 -0700

    working

--- diversity/summarize_seg_sites.R
+++ diversity/summarize_seg_sites.R
@@ -4,43 +4,23 @@ library(dplyr)
 
 source('../GenomeInfo.R')
 
-te=import.gff3($TEDISJOINED)
-
-$GENOME.segsites.txt
-
-m=fread(paste0(GENOME, '.segsites.txt'))
-m=m[m$V1 %in% 1:10,] ## get rid of contigs
-m$TEID=substr(m$V9, 4,24)
-
-m$segsites=m$V10
-
-
+te=fread(paste0(GENOME, '.segsites.txt'), sep='\t')
+te$TEID=substr(te$V9,4,24)
+flank=fread(paste0(GENOME, '.segsites.flank.txt'), sep='\t')
+flank$TEID=substr(flank$V9,4,24)
 
 ## get TE lengths to divide by
-tl=as.data.frame(te) %>% group_by(ID) %>% summarize(te_bp=sum(width, na.rm=T))
-names(tl)[1]='TEID'
+tl=as.data.frame(te) %>% group_by(TEID) %>% summarize(te_bp=sum(V5-V4+1, na.rm=T), segsites=sum(V10, na.rm=T))
 
-ms=full_join(m, tl)
-ms$fam=substr(ms$TEID,1,8)
-ms$sup=substr(ms$TEID,1,3)
+tl$segsites.bp=tl$segsites/tl$te_bp
 
-segsites=ms %>% group_by(TEID) %>% summarize(segsites=sum(segsites), te_bp=sum(te_bp), segsites.bp=sum(segsites)/sum(te_bp))
-segsites$sup=substr(segsites$TEID,1,3)
-segsites$fam=substr(segsites$TEID, 1,8)
+tl$fam=substr(tl$TEID,1,8)
+tl$sup=substr(tl$TEID,1,3)
 
+## although flank should be 1kb, at ends of chromosomes and contigs it can be shorter!
+tf=as.data.frame(flank) %>% group_by(TEID) %>% summarize(flank_bp=sum(V5-V4+1, na.rm=T), flank_segsites=sum(V10, na.rm=T))
+tf$flank_segsites.bp=tf$flank_segsites/tf$flank_bp
 
+segsites=merge(tl, tf, by='TEID', all=T)
 
-
-
-
-m=fread('B73v4_TE_segsites.FLANK1kbEACH.txt')
-m=m[m$V1 %in% 1:10,] ## get rid of contigs
-m$TEID=substr(m$V9, 4,24)
-
-m$segsites=m$V10
-
-segsites= m %>% group_by(TEID) %>% summarize(segsites_flank=sum(segsites), denominator=sum(V5-V4), segsites.bp_flank=sum(segsites)/sum(V5-V4))
-
-segsites$sup=substr(segsites$TEID,1,3)
-segsites$fam=substr(segsites$TEID, 1,8)
-
+write.table(segsites, paste0(GENOME, '.segregatingsites.TEandFlank.', Sys.Date(), '.txt'), quote=F, sep='\t', row.names=F, col.names=F)
