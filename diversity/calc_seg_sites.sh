f130f4332ca20b51f0a9ba35c400c962c1e2bfa0
mcstitzer <mcstitzer@ucdavis.edu>
2018-10-22 17:05:14 -0700

    do it all on scratch, make temp bed

--- diversity/calc_seg_sites.sh
+++ diversity/calc_seg_sites.sh
@@ -10,8 +10,11 @@ do
 ~/software/icommands/iget /iplant/home/shared/panzea/hapmap3/hmp321/unimputed/uplifted_APGv4/hmp321_agpv4_chr${i}.vcf.gz
 done
 
+zcat *.vcf.gz | awk '/^#/ {next} {print $1, $2-1, $2}' > hmp321_agpv4_segsites.bed
+sed -e 's/ */\t/g' <hmp321_agpv4_segsites.bed >hmp321_agpv4_segsites.tmp.bed
+mv hmp321_agpv4_segsites.tmp.bed hmp321_agpv4_segsites.bed
 
-bedtools intersect -c -a $TEDISJOINED -b hmp321_agpv4_chr1.vcf.gz hmp321_agpv4_chr2.vcf.gz hmp321_agpv4_chr3.vcf.gz hmp321_agpv4_chr4.vcf.gz hmp321_agpv4_chr5.vcf.gz hmp321_agpv4_chr6.vcf.gz hmp321_agpv4_chr7.vcf.gz hmp321_agpv4_chr8.vcf.gz hmp321_agpv4_chr9.vcf.gz hmp321_agpv4_chr10.vcf.gz > ~/projects/maize_genomic_ecosystem/diversity/$GENOME.segsites.txt
-bedtools intersect -c -a ~/projects/maize_genomic_ecosystem/mnase/B73.allTE.FLANK1kbEACH.gff3 -b hmp321_agpv4_chr1.vcf.gz hmp321_agpv4_chr2.vcf.gz hmp321_agpv4_chr3.vcf.gz hmp321_agpv4_chr4.vcf.gz hmp321_agpv4_chr5.vcf.gz hmp321_agpv4_chr6.vcf.gz hmp321_agpv4_chr7.vcf.gz hmp321_agpv4_chr8.vcf.gz hmp321_agpv4_chr9.vcf.gz hmp321_agpv4_chr10 > ~/projects/maize_genomic_ecosystem/diversity/$GENOME.segsites.flank.txt
+bedtools intersect -c -a $TEDISJOINED -b hmp321_agpv4_segsites.bed > ~/projects/maize_genomic_ecosystem/diversity/$GENOME.segsites.txt
+bedtools intersect -c -a ~/projects/maize_genomic_ecosystem/mnase/B73.allTE.FLANK1kbEACH.gff3 -b hmp321_agpv4_segsites.bed > ~/projects/maize_genomic_ecosystem/diversity/$GENOME.segsites.flank.txt
 
 rm -rf /scratch/mstitzer
