c0b615686fe081b2b6c911abf3803ada6465b919
Michelle Stitzer <mcstitzer@ucdavis.edu>
2018-10-31 17:20:36 -0700

    transdecoder

--- te_genes/orfs/run_transdecoder.sh
+++ te_genes/orfs/run_transdecoder.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -login
 
-~/software/TransDecoder-3.0.1/TransDecoder.Predict -t ../te_age/B73.allTE.fa --single_best_orf
+~/software/TransDecoder-3.0.1/TransDecoder.LongOrfs -m 20 -t ../../te_age/B73.eachTE.fa
+
+~/software/TransDecoder-3.0.1/TransDecoder.Predict -t ../../te_age/B73.eachTE.fa --single_best_orf
 
 Rscript filter_transdecoder.R
