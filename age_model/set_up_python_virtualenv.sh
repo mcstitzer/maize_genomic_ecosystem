ae6ee9369aa676158eda9aa4b40c35adb0a8dcb7
mcstitzer <mcstitzer@ucdavis.edu>
2018-11-29 13:02:40 -0800

    Create set_up_python_virtualenv.sh

---
+++ age_model/set_up_python_virtualenv.sh
@@ -0,0 +1,15 @@
+
+virtualenv .
+export PYTHONPATH=''
+pip install pandas
+pip install numpy
+pip install scipy
+pip install six
+pip install matplotlib lime
+pip install sklearn pdp pdpbox
+
+## to use:
+source bin/activate
+
+#to leave
+#deactivate
