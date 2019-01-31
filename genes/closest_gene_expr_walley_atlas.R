fe734ff70549e2046031662168a84829fe61a128
mcstitzer <mcstitzer@ucdavis.edu>
2019-01-31 15:16:01 -0800

    add gene tau, make nas 0

--- genes/closest_gene_expr_walley_atlas.R
+++ genes/closest_gene_expr_walley_atlas.R
@@ -9,7 +9,7 @@ tg=read.table('B73_closest_gene.2018-09-20.txt', header=T, sep='\t')
 ## this is gene expression
 #e=read.table('~/projects/b73_ecology/RawData/expression/gene_rpkm_31Aug17.txt.gz', header=T)
 e=fread('walley_fpkm.txt') ## from https://ftp.maizegdb.org/MaizeGDB/FTP/B73_RefGen_v4%20RNA-SEQ%20Gene%20Atlas/walley_fpkm.txt
-
+e[is.na(e)]=0
 
 
 ## unfortunately, not every TE has a nearby gene we can work with
@@ -18,8 +18,6 @@ tge[,colnames(e)]=e[e$geneID %in% tg$closestgene,][match(tg$closestgene[tg$close
 
 ## the m ending denotes using em, the mean expression across all replicates
 
-tge_famsize=as.numeric(table(tge$fam)[tge$fam])
-tge$famsize=tge_famsize
 
 colnames(tge)[8:30]=gsub(' ', '_', colnames(tge)[8:30])   
 colnames(tge)[8:30]=gsub('/', '_', colnames(tge)[8:30]) 
@@ -33,6 +31,16 @@ coef.variation <- function(x) {
 }
 tge$gene_coefvar=sapply(1:nrow(tge), function(x) coef.variation(unlist(tge[x,8:30])))                             
 tge$gene_median=sapply(1:nrow(tge), function(x) median(unlist(tge[x,8:30]), na.rm=T))                             
-                             
+    
+## calculate gene tau
+tau=function(x){
+    t=sum(1-x/max(x))/(length(x)-1)
+  }
+tge$gene_tau=apply(tge[,8:30], 1, tau)
+                       
+                       
+tge_famsize=as.numeric(table(tge$fam)[tge$fam])
+tge$famsize=tge_famsize
+                       
 write.table(tge, 'B73_closest_gene_expression.maizegdbWalley.txt', row.names=F, col.names=T, sep='\t')                             
                      
