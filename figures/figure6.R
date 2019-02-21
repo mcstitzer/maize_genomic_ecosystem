a878b8534e2f161afc21d8600fcc898f639155e2
mcstitzer <mcstitzer@ucdavis.edu>
2019-02-20 20:29:51 -0800

    back to where it started

--- figures/figure6.R
+++ figures/figure6.R
@@ -11,6 +11,11 @@ ind$mya=ind$age/2/3.3e-8/1e6
 imp=fread('../age_model/importance_each_variable.2019-01-31.txt')
 impsort=fread('../age_model/importance_combo_variable.2019-01-31.txt')
 
+## get rmse in units of mya
+imp$rmseMya
+imp$rmseMya=sqrt(imp$X.IncMSE/100)/2/3.3e-8/1e6
+impsort$rmseMya=sqrt(impsort$sum/100)/2/3.3e-8/1e6 ## should i be doing this before summing though?
+
 ## make a color scale for importance plots!
 myColors <- brewer.pal(12,"Paired")
 myColors=myColors[c(1:6,8,10,11,12)]
@@ -48,13 +53,72 @@ imp$category=mapvalues(imp$feat, from=categories$feature, to=categories$category
 #meltimpsum=melt(imp %>% group_by(category) %>% summarize_if(.predicate="is.numeric", .funs="abs") %>% summarize_if(.predicate="is.numeric", .funs="sum") %>% arrange(desc(X.IncMSE))) #sum=sum(X.IncMSE), meanscaled=mean(scaled)) %>% arrange(desc(sum))
 #meltimpall=melt(imp[rev(order(abs(imp$X.IncMSE))),]) ## keep this managable!
 meltimp=melt(imp[rev(order(abs(imp$scaled))),-'X.IncMSE'][1:30,]) ## keep this managable!
-meltimpsum=melt(imp %>% group_by(category) %>% summarize_if(.predicate="is.numeric", .funs="sum") %>% arrange(desc(scaled))) #sum=sum(X.IncMSE), meanscaled=mean(scaled)) %>% arrange(desc(sum))
+meltimpsum=melt(imp %>% group_by(category) %>% summarize_if(.predicate="is.numeric", .funs="sum", na.rm=TRUE) %>% arrange(desc(scaled))) #sum=sum(X.IncMSE), meanscaled=mean(scaled)) %>% arrange(desc(sum))
 meltimpall=melt(imp[rev(order(abs(imp$scaled))),]) ## keep this managable!
-meltimpmean=melt(imp %>% group_by(category) %>% summarize_if(.predicate="is.numeric", .funs="mean") %>% arrange(desc(scaled))) #sum=sum(X.IncMSE), meanscaled=mean(scaled)) %>% arrange(desc(sum))
+meltimpmean=melt(imp %>% group_by(category) %>% summarize_if(.predicate="is.numeric", .funs="mean", na.rm=TRUE) %>% arrange(desc(scaled))) #sum=sum(X.IncMSE), meanscaled=mean(scaled)) %>% arrange(desc(sum))
 
 
-############ start plotting
+## for text:
+library(stringr)
+melt(imp %>% group_by(str_split_fixed(category, '_', 2)[,1]) %>% summarize_if(.predicate="is.numeric", .funs="mean", na.rm=TRUE) %>% arrange(desc(scaled))) #sum=sum(X.IncMSE), meanscaled=mean(scaled)) %>% arrange(desc(sum))
+melt(imp %>% group_by(str_split_fixed(category, '_', 2)[,1]) %>% summarize_if(.predicate="is.numeric", .funs="sum", na.rm=TRUE) %>% arrange(desc(scaled))) #sum=sum(X.IncMSE), meanscaled=mean(scaled)) %>% arrange(desc(sum))
+
+
+## real plot
 pdf(paste0('figure6.modeloutput.', Sys.Date(), '.pdf'), 30,12)
+
+ispsc=ggplot(meltimp[meltimp$variable=='rmseMya',], aes(x=feat, y=abs(value), fill=category)) + geom_bar(position="dodge",stat="identity") + 
+#       geom_errorbar(aes(ymin=weight-std/2, ymax=weight+std/2), size=.3, width=.2, position=position_dodge(.9)) +
+       scale_x_discrete(limits=meltimp$feat[order(meltimp[meltimp$variable=='rmseMya','value'])]) +  
+       coord_flip() + #scale_fill_brewer(palette='Set3') #+ colScale
+       scale_fill_manual(values=myColors)
+
+musc=ggplot(meltimpmean[meltimpmean$variable=='rmseMya',], aes(x=category, y=abs(value), fill=category)) + geom_bar(position="dodge",stat="identity") + 
+#       geom_errorbar(aes(ymin=weight-std/2, ymax=weight+std/2), size=.3, width=.2, position=position_dodge(.9)) +
+       scale_x_discrete(limits=meltimpmean$category[order(abs(meltimpmean[meltimpmean$variable=='rmseMya','value']))]) +  
+       coord_flip() + #scale_fill_brewer(palette='Set3') #+ colScale
+       scale_fill_manual(values=myColors)
+
+## try out some model fits!
+ss=fread('../age_model/segsites.bp.2019-01-31.txt')
+mSSf=ggplot(ss, aes(segsites.bp, yhat.centered/2/3.3e-8/1e6, color=sup)) + 
+                geom_line(aes(group = yhat.id), alpha = 0.1, data=ss[ss$yhat.id %in% sample(1:34151, 1000),]) + 
+#                stat_summary(fun.y = mean, geom = "line",  size = 0.5, aes(group=fam, color=sup), alpha=0.2)+  
+                stat_summary(fun.y = mean, geom = "line",  size = 2, aes(group=sup, color=sup))+  
+                scale_color_manual(values=dd.col)
+anth=fread('../age_model/anther_avg_chh.2019-02-01.txt')
+mAnthf=ggplot(anth, aes(anther_avg_chh, yhat.centered/2/3.3e-8/1e6, color=sup)) + #
+                geom_line(aes(group = yhat.id), alpha = 0.1, data=anth[anth$yhat.id %in% sample(1:34151, 1000),]) + 
+#                stat_summary(fun.y = mean, geom = "line",  size = 0.5, aes(group=fam, color=sup), alpha=0.2)+  
+                stat_summary(fun.y = mean, geom = "line",  size = 2, aes(group=sup, color=sup))+  
+                scale_color_manual(values=dd.col)
+
+## plot the raw correlations
+rSS=ggplot(ind, aes(x=segsites.bp, y=mya, color=sup)) + geom_point(size=0.1, alpha=0.1) + stat_smooth() + ylim(0,1) + theme(legend.position='none') + scale_color_manual(values=dd.col) + xlim(0,0.05)
+rD=ggplot(ind, aes(group=disruptor, y=mya, color=sup)) + geom_boxplot() + ylim(0,1) + theme(legend.position='none')+ scale_color_manual(values=dd.col)
+rAnth=ggplot(ind, aes(x=anther_avg_chh, y=mya, color=sup)) + geom_point(size=0.1, alpha=0.1) + stat_smooth() + ylim(0,1) + theme(legend.position='none')+ scale_color_manual(values=dd.col)+ xlim(0,0.05)
+rP=ggplot(ind, aes(x=TEfam_pollen_mature, y=mya, color=sup)) + geom_point(size=0.1, alpha=0.1) + stat_smooth() + ylim(0,1) + theme(legend.position='none')+ scale_color_manual(values=dd.col) + xlim(0,2000)
+
+r=plot_grid(rSS, mSSf + ylim(0,5), rAnth, mAnthf + ylim(0,1), labels=c('C', 'D', 'E', 'F'), ncol=2, align='v')
+
+
+plot_grid(musc + theme(legend.position='none') + xlab('Reduction in square root mean squared error (Mya)') + ylab(''), 
+          ispsc + theme(legend.position='none') + xlab('Reduction in square root mean squared error (Mya)') + ylab(''), 
+          r, 
+          ncol = 3, labels=c('A', 'B', ''), align = 'v', rel_widths=c(1,1,1.5))
+          
+## supp
+#plot_grid(ispbysupscxM+ theme(legend.position='none'), ispbysupscxN+ theme(legend.position='none'), labels = "AUTO", ncol = 2, align = 'v', rel_widths=c(0.3,1))
+
+dev.off()
+
+
+
+
+
+## old stuff!
+############ start plotting
+##pdf(paste0('figure6.modeloutput.', Sys.Date(), '.pdf'), 30,12)
 ip=ggplot(impsort, aes(y=category, x=sum)) + geom_point() + scale_y_discrete(limits=impsort$category)
 #isp=ggplot(impsort[impsort$meanscaled>1.9,], aes(y=category, x=meanscaled)) + geom_point() + 
 #       scale_y_discrete(limits=rev(impsort[impsort$meanscaled>1.9,] %>% arrange(desc(meanscaled)))$category)
